0368ca07f919450611547b2727f4debc052702cf
lifuchun <01118311@wisedu.com>
2021-07-04 13:49:27 +0800

    master change first time

---
+++ src/test/Another.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Another {
+    public static void main(String[] args) {
+        System.out.println("另一个修改");
+    }
+}
--- src/test/Main.java
+++ src/test/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
              System.out.println("master change first time");
+
+             System.out.println("dev change first time");
     }
 
 }
